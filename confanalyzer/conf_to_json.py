15b3da0832bbf0523d2371ab0f566d2876666356
Draevich Matvey <mdraevich@solidex.by>
2021-05-19 14:03:53 +0300

    delete excessive code

--- confanalyzer/conf_to_json.py
+++ confanalyzer/conf_to_json.py
@@ -376,223 +376,34 @@ def _get_from_config(source, **kwargs):
     answer = [ line.replace('\\', '') for line in file.readlines() ]  # remove "\" char from the configuration
     answer = _correct_vdom_sections(answer)
     answer = _update_vdom_sections(answer)
-    answer = (answer, list(range(len(answer))))
 
     #
-    for block_type in [ "vdom", "config", "edit" ]:
-        answer = _get_block(content=answer, block_of=block_type, block_args=kwargs.get(block_type))
-    #
-    if kwargs.get("leaves"):  # there are some leaves to filter
-        lines, references = [], []
-        for leaf in kwargs.get("leaves"):
-            l, r = _get_block(content=answer, block_of="set", block_args=leaf)
-            lines += l
-            references += r 
-        answer = (lines, references)
-    #
-    if answer[0]:
-        if kwargs.get("leaves"):
-            answer = _wrap_in_block(content=answer, block_of="edit", block_args=kwargs.get("edit"))
-        if kwargs.get("edit"):
-            answer = _wrap_in_block(content=answer, block_of="config", block_args=kwargs.get("config"))
-        if kwargs.get("config") and kwargs.get("vdom"):
-            answer = _wrap_in_block(content=answer, block_of="vdom", block_args=kwargs.get("vdom"))
-    #
-    return _from_cli_to_object(answer[0])
-
-
-def _set_to_config_from_yaml(source, yaml_filename, **kwargs):
-    yaml_file = open_carefully(yaml_filename, "r")
-    #
-    rendered_yaml = "\n".join([ Template(line).render(kwargs['jinja2']) for line in yaml_file.readlines() ])
-    obj = yaml.load(rendered_yaml, Loader=yaml.FullLoader)
-    return obj
-
-
-def _set_to_config_by_args(source, **kwargs):
-    in_file = open_carefully(source, mode="r")
-    #
-    vdom, config, edit, leaves = [ kwargs.get(block_of) for block_of in [ "vdom", "config", "edit", "leaves" ] ]
-    if leaves:
-        del kwargs["leaves"]
-    #
-    requested_object = {}
-    modify_object = requested_object
-    #
-    if vdom:
-        if modify_object.get(vdom) is None:
-            modify_object[vdom] = [[{}]]
-        modify_object = modify_object[vdom][0][0]  # dive into
-    #
-    if config:
-        if modify_object.get(config) is None:
-            modify_object[config] = [{}]
-        modify_object = modify_object[config][0]  # dive into
-    #
-    if edit:
-        if modify_object.get(edit) is None:
-            modify_object[edit] = {}
-        modify_object = modify_object[edit]  # dive into
-    #
-    if leaves:
-        for leaf in leaves:
-            modify_object[leaf] = leaves[leaf] 
-    #
-    return requested_object
-
-
-def _merge_objects(from_obj, to_obj):
-    if isinstance(from_obj, list):
-        return [ _merge_objects(from_obj[0], to_obj[0]) ]
-    if isinstance(from_obj, str):
-        return from_obj
-    #
-    return_object = copy.deepcopy(to_obj)
-    for element in from_obj:
-        if to_obj.get(element) is None:
-            return_object[element] = copy.deepcopy(from_obj[element])
-        else:
-            return_object[element] = _merge_objects(from_obj[element], to_obj[element])
-    #
-    return return_object
-
-
-def _find_in_config(source, **kwargs):
-    in_file = open_carefully(source, mode="r")
-    #
-    if in_file is None:
-        return "No such file :("
-    #
-    source_config = in_file.readlines()
-    answer = (source_config, list(range(len(source_config))))  # answer[0] is lines, answer[1] is reference numbers
-    #
-    for block_type in [ "vdom", "config", "edit" ]:
-        answer = _get_block(content=answer, block_of=block_type, block_args=kwargs.get(block_type))
-    #
-    if kwargs.get('leaves') is not None:  # there are some leaves to filter
-        lines, references = [], []
-        for leaf in kwargs.get("leaves"):
-            l, r = _get_block(content=answer, block_of="set", block_args=leaf)
-            lines += l
-            references += r 
-        answer = (lines, references)
-    #
-    return answer
-
-
-def _set_to_config(source, destination, yaml, **kwargs):
-    requested_object = None
-    #
-    if destination is None:
-        destination = "{}.conf".format(str(uuid.uuid4().hex))
-    #
-    if yaml:
-        requested_object = _set_to_config_from_yaml(source, yaml, **kwargs)
-    else:
-        requested_object = _set_to_config_by_args(source, **kwargs)
-    #
-    if requested_object is None:
-        return False
-    #
-    vdom_mode = False
-    keys = list(requested_object.keys())
-    if not keys:
-        return False
-    # 
-    if isinstance(requested_object[keys[0]][0], list):
-        vdom_mode = True
-    #
-    existing_object = {}
-    if vdom_mode:
-        for vdom in requested_object:
-            for config in requested_object[vdom][0][0]:
-                config_in_vdom = _get_from_config(source, vdom=vdom, config=config)
-                if config_in_vdom:
-                    if existing_object.get(vdom) is None:
-                        existing_object[vdom] = [[{}]]
-                    existing_object[vdom][0][0][config] = config_in_vdom[vdom][0][0][config]
-    else:
-        for config in requested_object:
-            config_in = _get_from_config(source, config=config)
-            if config_in:
-                existing_object[config] = config_in[config]
-    #
-    existing_object = _merge_objects(requested_object, existing_object)
-    #
-    #
-    in_file = open_carefully(source, mode="r")
-    lines = in_file.readlines()
-    in_file.close()
-    #
-    out_file = open_carefully(destination, mode="w")
-    out_file.writelines(lines)
-    out_file.close()
-    source = destination
-    #
-    #
-    #
-    if vdom_mode:
-        for vdom in existing_object:
-            for config in existing_object[vdom][0][0]:
-                ref = _find_in_config(destination, vdom=vdom, config=config)[1]
-                #
-                in_file = open_carefully(destination, mode="r")
-                lines = in_file.readlines() 
-                in_file.close()
-                #
-                if ref:
-                    mn, mx = ref[0], ref[-1]
-                    replace = [ el + "\n" for el in _from_object_to_cli(existing_object[vdom][0][0][config][0], spaces=4) ]
-                    lines = lines[:mn+1] + replace + lines[mx:]
-                else: 
-                    mx = _find_in_config(destination, vdom=vdom)[1][-1]
-                    wrapped_obj = { config: [ existing_object[vdom][0][0][config][0] ] }
-                    replace = [ el + "\n" for el in _from_object_to_cli(wrapped_obj, spaces=0) ]
-                    lines = lines[:mx] + replace + lines[mx:]
-                #
-                out_file = open_carefully(destination, mode="w")
-                out_file.writelines(lines) 
-                out_file.close()
-    else:
-        pass  # add instructions here
-    #
-    return requested_object
-
-
-def _delete_from_config(source, destination, **kwargs):
-    in_file = open_carefully(source, mode="r")
-    if destination is None:
-        destination = "{}.conf".format(str(uuid.uuid4().hex))
-    #
-    if in_file is None:
-        return "No such file :("
-    #
-    source_config = in_file.readlines()
-    in_file.close()
-    #
-    answer = _find_in_config(source, **kwargs)
-    #
-    # need check if object is not mentioned in other objects
-    #
-    out_file = open_carefully(destination, mode="w")
-    ref_counter = 0
-    for line in source_config:
-        if ref_counter not in answer[1]:
-            out_file.write(line)
-        ref_counter += 1
-    return destination
+    # for block_type in [ "vdom", "config", "edit" ]:
+    #     answer = _get_block(content=answer, block_of=block_type, block_args=kwargs.get(block_type))
+    # #
+    # if kwargs.get("leaves"):  # there are some leaves to filter
+    #     lines, references = [], []
+    #     for leaf in kwargs.get("leaves"):
+    #         l, r = _get_block(content=answer, block_of="set", block_args=leaf)
+    #         lines += l
+    #         references += r 
+    #     answer = (lines, references)
+    # #
+    # if answer[0]:
+    #     if kwargs.get("leaves"):
+    #         answer = _wrap_in_block(content=answer, block_of="edit", block_args=kwargs.get("edit"))
+    #     if kwargs.get("edit"):
+    #         answer = _wrap_in_block(content=answer, block_of="config", block_args=kwargs.get("config"))
+    #     if kwargs.get("config") and kwargs.get("vdom"):
+    #         answer = _wrap_in_block(content=answer, block_of="vdom", block_args=kwargs.get("vdom"))
+    # #
+    return _from_cli_to_object(answer)
 
 
 
 def _proccess_request(action, **kwargs):
     if action == "get":
         return _get_from_config(**kwargs)
-    if action == "delete":
-        return _delete_from_config(**kwargs)
-    if action == "find":
-        return _find_in_config(**kwargs)
-    if action == "set":
-        return _set_to_config(**kwargs)
 
 
 def run_module(src):
