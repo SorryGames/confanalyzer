283dd05d124b4f847368405a646c133ae0f9d27e
Draevich Matvey <mdraevich@solidex.by>
2021-05-19 14:06:47 +0300

    delete excessive code

--- confanalyzer/conf_to_json.py
+++ confanalyzer/conf_to_json.py
@@ -62,62 +62,6 @@ def _standard_form(content):
     return content
 
 
-def _from_object_to_cli(obj, spaces=0): 
-    mode = None
-    return_object = []
-    for element in obj: 
-        if isinstance(obj[element], list):
-            if isinstance(obj[element][0], list):
-                mode = "vdom"  # [[ {} ]]
-            else:
-                mode = "config"  # [ {} ]
-        elif isinstance(obj[element], dict):
-            mode = "edit"  # {}
-        else:
-            mode = "set"  # element: obj[element] 
-        #
-        if mode == "vdom":
-            if element == "global":
-                return_object += [
-                    " " * spaces + "config {}".format(element),
-                    *_from_object_to_cli(obj[element][0][0], spaces+TAB_WIDTH),
-                    " " * spaces + "end"
-                ]
-            else:
-                return_object += [
-                    " " * spaces + "config vdom",
-                    " " * spaces + "edit {}".format(element),
-                    *_from_object_to_cli(obj[element][0][0], spaces+TAB_WIDTH),
-                    " " * spaces + "end"
-                ]
-        #        
-        if mode == "config":
-            return_object += [
-                " " * spaces + "config {}".format(element),
-                *_from_object_to_cli(obj[element][0], spaces+TAB_WIDTH),
-                " " * spaces + "end"
-            ]
-        if mode == "edit":
-            return_object += [
-                " " * spaces + 'edit "{}"'.format(element),
-                *_from_object_to_cli(obj[element], spaces+TAB_WIDTH),
-                " " * spaces + "next"
-            ]
-        if mode == "set":
-            template = " " * spaces + "set {} {}"
-            if element in NEED_QUOTES:
-                template = " " * spaces + 'set {} "{}"' 
-            if element in NEED_MULTIPLE_QUOTES:
-                template = " " * spaces + 'set {}'.format(element)
-                for option in obj[element].split():
-                    template += (' "{}"'.format(option)).replace("#", " ")
-            else:
-                template = template.format(element, obj[element])
-            return_object += [ template ]
-    return return_object
-
-
-
 def _from_cli_to_object(content):
     python_content = [ "{" ]
     id_counter = 0
@@ -153,34 +97,6 @@ def _from_cli_to_object(content):
     python_content += [ "}" ]
     python_content = " ".join(python_content)
     return eval(python_content)
-
-
-def _wrap_in_block(content, block_of, block_args):
-    if block_args is None:
-        return content
-    #
-    lines, references = [], []
-    before, after = [], []
-    block_map = {
-        "config": "end",
-        "edit": "next",
-        "set": "set",
-        "vdom": "close"
-    }
-    #
-    before.append("{} {}\n".format(block_of, block_args))
-    after.append("{}\n".format(block_map[block_of]))
-    #
-    lines += before
-    references += [ -1 ] * len(before)
-    #
-    lines += content[0]
-    references += content[1]
-    #
-    lines += after
-    references += [ -1 ] * len(after)
-    #
-    return (lines, references)
 
 
 def _update_vdom_sections(content):
@@ -288,78 +204,6 @@ def _correct_vdom_sections(content):
     return content_b
 
 
-
-def _get_block(content, block_of, block_args):
-
-    if block_args is None:
-        return content
-    #
-    block_counter = 0
-    global_counter = 0
-    detected_replacemsg = False
-    block_args = block_args.split()  # "system console" -> "[ "system", "console" ]"
-    #
-    if block_of == "vdom" and block_args == [ "global" ]:
-        block_of = "config"
-    #
-    content_b = []
-    block_reference = []
-    block_map = {
-        "config": "end",
-        "edit": "next",
-        "set": "set",
-        "vdom": "end"
-    }
-    #
-    block_end = block_map[block_of]
-    #
-    for index in range(len(content[0])):
-        line = _standard_form(content[0][index])  # line = [ "config", "system", "console" ]
-        #
-        if line == []:
-            continue
-        #
-        if line[0] in [ "config", "edit" ]:
-            global_counter += 1
-        if line[0] in [ "end", "next" ]:
-            global_counter -= 1
-        #
-        # if line[:3] == [ "config", "system", "replacemsg" ]:
-        #     detected_replacemsg = True
-        # if line[0] == "end":
-        #     detected_replacemsg = False
-        # if detected_replacemsg:
-        #     continue
-
-        #
-        if block_counter and line[0] == block_of:
-            block_counter += 1
-        #
-        compose_line = [ block_of ] + block_args
-        if line[:len(compose_line)] == compose_line:
-            block_counter += 1
-        if block_counter and block_of == "vdom" and line[0] == "config":
-            block_counter += 1
-        #
-        if block_counter and global_counter == 1 and block_of == "set":
-            content_b.append(content[0][index])
-            block_reference.append(content[1][index])
-        if block_counter and block_of in [ "config", "edit", "vdom" ]:
-            content_b.append(content[0][index])
-            block_reference.append(content[1][index])
-        #
-        if block_counter == 1 and block_of == "vdom":
-            if global_counter == 1:  # edit root
-                content_b[-1] = "vdom" + content_b[-1][4:]
-            if global_counter == 0:  # end
-                content_b[-1] = "close" + content_b[-1][3:]
-        #
-        if line[0] == block_end and block_counter: 
-            block_counter -= 1
-    #
-    return (content_b, block_reference)
-
-
 def open_carefully(filename, mode="r"):
     try:
         return open(filename, mode)
@@ -376,27 +220,7 @@ def _get_from_config(source, **kwargs):
     answer = [ line.replace('\\', '') for line in file.readlines() ]  # remove "\" char from the configuration
     answer = _correct_vdom_sections(answer)
     answer = _update_vdom_sections(answer)
-
     #
-    # for block_type in [ "vdom", "config", "edit" ]:
-    #     answer = _get_block(content=answer, block_of=block_type, block_args=kwargs.get(block_type))
-    # #
-    # if kwargs.get("leaves"):  # there are some leaves to filter
-    #     lines, references = [], []
-    #     for leaf in kwargs.get("leaves"):
-    #         l, r = _get_block(content=answer, block_of="set", block_args=leaf)
-    #         lines += l
-    #         references += r 
-    #     answer = (lines, references)
-    # #
-    # if answer[0]:
-    #     if kwargs.get("leaves"):
-    #         answer = _wrap_in_block(content=answer, block_of="edit", block_args=kwargs.get("edit"))
-    #     if kwargs.get("edit"):
-    #         answer = _wrap_in_block(content=answer, block_of="config", block_args=kwargs.get("config"))
-    #     if kwargs.get("config") and kwargs.get("vdom"):
-    #         answer = _wrap_in_block(content=answer, block_of="vdom", block_args=kwargs.get("vdom"))
-    # #
     return _from_cli_to_object(answer)
 
 
